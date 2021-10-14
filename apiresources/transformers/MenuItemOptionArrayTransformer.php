bdb30eaf6fd526409ccbd9e66ff22afc6998b02f
Justin v Opijnen <j.vanopijnen@octoblue.nl>
2021-10-14 14:15:55 +0200

    refactored MenuItemOptionArrayTransformer

--- apiresources/transformers/MenuItemOptionArrayTransformer.php
+++ apiresources/transformers/MenuItemOptionArrayTransformer.php
@@ -5,12 +5,9 @@ namespace Igniter\Api\ApiResources\Transformers;
 use Admin\Models\Menu_item_options_model;
 use League\Fractal\TransformerAbstract;
 
-class MenuItemOptionArrayTransformer extends TransformerAbstract
+class MenuItemOptionArrayTransformer extends MenuItemOptionTransformer
 {
-    protected $defaultIncludes = [
-        'menu_option_values',
-    ];
-
+    
     public function transform(array $menuItemOption)
     {
         return array_merge($menuItemOption, [
@@ -19,21 +16,4 @@ class MenuItemOptionArrayTransformer extends TransformerAbstract
         ]);
     }
 
-    public function includeMenuOptionValues(Menu_item_options_model $menuItemOption)
-    {
-        //When Post/Patch and inside body comes with an json array option_values the deserialized object is a collection of array
-        if (is_array($menuItemOption->menu_option_values)){
-            return $this->collection(
-                $menuOption->menu_option_values,
-                new MenuItemOptionValueArrayTransformer,
-                'menu_option_values'
-            );
-        }
-
-        return $this->collection(
-            $menuItemOption->menu_option_values,
-            new MenuItemOptionValueTransformer,
-            'menu_option_values'
-        );
-    }
 }
